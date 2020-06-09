dbecc1ab13fa7eb0491ea84f90ea90d21e74b7d5
masterUNG <phrombutr@gmail.com>
2020-06-10 05:57:00 +0700

    update 10 June

--- lib/screens/add_food_menu.dart
+++ lib/screens/add_food_menu.dart
@@ -1,5 +1,13 @@
+import 'dart:io';
+import 'dart:math';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:ungfood/utility/my_constant.dart';
 import 'package:ungfood/utility/my_style.dart';
+import 'package:ungfood/utility/normal_dialog.dart';
 
 class AddFoodMenu extends StatefulWidget {
   @override
@@ -7,6 +15,9 @@ class AddFoodMenu extends StatefulWidget {
 }
 
 class _AddFoodMenuState extends State<AddFoodMenu> {
+  File file;
+  String nameFood, price, detail;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,17 +44,67 @@ class _AddFoodMenuState extends State<AddFoodMenu> {
   }
 
   Widget saveButton() {
-    return Container(width: MediaQuery.of(context).size.width,
-      child: RaisedButton.icon(color: MyStyle().primaryColor,
-                onPressed: () {},
-                icon: Icon(Icons.save, color: Colors.white,),
-                label: Text('Save Food Menu', style: TextStyle(color: Colors.white),),),
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      child: RaisedButton.icon(
+        color: MyStyle().primaryColor,
+        onPressed: () {
+          if (file == null) {
+            normalDialog(context,
+                'กรุณาเลือกรูปภาพ อาหาร โดยการ Tap Camera หรือ Gallery');
+          } else if (nameFood == null ||
+              nameFood.isEmpty ||
+              price == null ||
+              price.isEmpty ||
+              detail == null ||
+              detail.isEmpty) {
+            normalDialog(context, 'กรุณากรอก ทุกช่อง คะ');
+          } else {
+            uploadFoodAndInsertData();
+          }
+        },
+        icon: Icon(
+          Icons.save,
+          color: Colors.white,
+        ),
+        label: Text(
+          'Save Food Menu',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
     );
+  }
+
+  Future<Null> uploadFoodAndInsertData() async {
+    String urlUpload = '${MyConstant().domain}/UngFood/saveFood.php';
+
+    Random random = Random();
+    int i = random.nextInt(1000000);
+    String nameFile = 'food$i.jpg';
+
+    try {
+      Map<String, dynamic> map = Map();
+      map['file'] = await MultipartFile.fromFile(file.path, filename: nameFile);
+      FormData formData = FormData.fromMap(map);
+
+      await Dio().post(urlUpload, data: formData).then((value) async {
+        String urlPathImage = '/UngFood/Food/$nameFile';
+        print('urlPathImage = ${MyConstant().domain}$urlPathImage');
+
+        SharedPreferences preferences = await SharedPreferences.getInstance();
+        String idShop = preferences.getString('id');
+
+        String urlInsertData =
+            '${MyConstant().domain}/UngFood/addFood.php?isAdd=true&idShop=$idShop&NameFood=$nameFood&PathImage=$urlPathImage&Price=$price&Detail=$detail';
+        await Dio().get(urlInsertData).then((value) => Navigator.pop(context));
+      });
+    } catch (e) {}
   }
 
   Widget nameForm() => Container(
         width: 250.0,
         child: TextField(
+          onChanged: (value) => nameFood = value.trim(),
           decoration: InputDecoration(
             prefixIcon: Icon(Icons.fastfood),
             labelText: 'ชื่ออาหาร',
@@ -55,6 +116,8 @@ class _AddFoodMenuState extends State<AddFoodMenu> {
   Widget priceForm() => Container(
         width: 250.0,
         child: TextField(
+          keyboardType: TextInputType.number,
+          onChanged: (value) => price = value.trim(),
           decoration: InputDecoration(
             prefixIcon: Icon(Icons.attach_money),
             labelText: 'ราคาอาหาร',
@@ -66,6 +129,7 @@ class _AddFoodMenuState extends State<AddFoodMenu> {
   Widget detailForm() => Container(
         width: 250.0,
         child: TextField(
+          onChanged: (value) => detail = value.trim(),
           keyboardType: TextInputType.multiline,
           maxLines: 3,
           decoration: InputDecoration(
@@ -80,15 +144,36 @@ class _AddFoodMenuState extends State<AddFoodMenu> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
-        IconButton(icon: Icon(Icons.add_a_photo), onPressed: null),
+        IconButton(
+          icon: Icon(Icons.add_a_photo),
+          onPressed: () => chooseImage(ImageSource.camera),
+        ),
         Container(
           width: 250.0,
           height: 250.0,
-          child: Image.asset('images/foodmenu.png'),
+          child: file == null
+              ? Image.asset('images/foodmenu.png')
+              : Image.file(file),
         ),
-        IconButton(icon: Icon(Icons.add_photo_alternate), onPressed: null),
+        IconButton(
+          icon: Icon(Icons.add_photo_alternate),
+          onPressed: () => chooseImage(ImageSource.gallery),
+        ),
       ],
     );
+  }
+
+  Future<Null> chooseImage(ImageSource source) async {
+    try {
+      var object = await ImagePicker().getImage(
+        source: source,
+        maxWidth: 800.0,
+        maxHeight: 800.0,
+      );
+      setState(() {
+        file = File(object.path);
+      });
+    } catch (e) {}
   }
 
   Widget showTitleFood(String string) {
